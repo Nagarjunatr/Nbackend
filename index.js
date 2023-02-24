0362e25f06f6056f5276014150e6ea36633d26fc
Nagarjunatr <103265658+Nagarjunatr@users.noreply.github.com>
2023-02-24 18:49:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,4 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
 })
